88a77bb76adeb1e9f58fdf81d5930bcc81370096
caua-carvalho <caua.carma033@gmail.com>
2025-12-04 16:10:47 -0800

    formatação da resposta do controllerpara json

--- public/index.php
+++ public/index.php
@@ -1,4 +1,5 @@
 <?php
+ob_start(); // inicia buffer de saída
 
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../app/config/config.php';
@@ -10,4 +11,6 @@ $router = new Router();
 // Carregar rotas
 require_once __DIR__ . '/../routes/api.php';
 
+header('Content-Type: application/json; charset=utf-8');
 $router->dispatch();
+ob_end_flush();
