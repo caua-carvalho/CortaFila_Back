6e4b2379844df0d973d501deef308e6ceb6b7603
caua-carvalho <caua.carma033@gmail.com>
2025-12-11 06:54:08 -0800

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,4 +1,14 @@
 <?php
+// Permite qualquer origem (pode restringir depois)
+header("Access-Control-Allow-Origin: https://corta-fila.onrender.com");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+
+// Responde imediatamente a requisições OPTIONS (pré-flight)
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(204);
+    exit();
+}
 
 require __DIR__ . '/../vendor/autoload.php';
 
