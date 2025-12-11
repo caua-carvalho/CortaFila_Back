aa7deca0df6025e78283c95f592962e443db8191
caua-carvalho <caua.carma033@gmail.com>
2025-12-10 18:22:39 -0800

    Update EmployeeController.php

--- app/controllers/EmployeeController.php
+++ app/controllers/EmployeeController.php
@@ -8,6 +8,7 @@ use App\Core\RequestContext;
 class EmployeeController
 {
     private EmployeeService $service;
+    private EmployeeService $invite;
 
     public function __construct()
     {
@@ -27,5 +28,24 @@ class EmployeeController
         );
 
         echo json_encode($result);
+    }
+
+    public function activateEmployee()
+    {
+        $body = json_decode(file_get_contents('php://input'), true);
+
+        $password = $body['password'];
+        $token = $body['token'];
+
+        $result = $this->service->activateEmployee($token, $password);
+
+        echo json_encode($result);
+    }
+
+    public function findEmployeeByToken($token)
+    {
+        $user = $this->service->findByToken($token['token']);
+
+        echo json_encode($user);
     }
 }
