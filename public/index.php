5e9ab7c0c711bdcbc6c609b5003695084ac8f989
caua-carvalho <caua.carma033@gmail.com>
2025-12-11 15:48:55 -0800

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 // Permite qualquer origem (pode restringir depois)
-header("Access-Control-Allow-Origin: https://corta-fila.onrender.com");
+header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 
